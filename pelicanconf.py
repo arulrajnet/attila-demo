a5c97e5ed8f7a636a48dc9eec0ee1ee757b17979
Arul <me@arulraj.net>
2019-04-14 19:36:57 +0530

    fix for pelilcan 4

--- pelicanconf.py
+++ pelicanconf.py
@@ -142,3 +142,15 @@ AUTHORS_BIO = {
 COLOR_SCHEME_CSS = 'github.css'
 
 CSS_OVERRIDE = ['assets/css/myblog.css']
+
+# Jinja config - Pelican 4
+JINJA_ENVIRONMENT = {
+  'extensions' :[
+    'jinja2.ext.loopcontrols',
+    'jinja2.ext.i18n',
+    'jinja2.ext.with_',
+    'jinja2.ext.do'
+  ]
+}
+
+JINJA_FILTERS = {'max': max}
