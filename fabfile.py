a2874ad9eadc5fb788fbc6d467a33ed4fac0cf18
Arul <me@arulraj.net>
2022-09-19 12:22:08 +0530

    Update to fabric2
    - readme updated
    
    - remove jinja extensions

--- fabfile.py
+++ fabfile.py
@@ -1,95 +1,75 @@
-from fabric.api import *
-import fabric.contrib.project as project
+from fabric import *
+from fabric.connection import *
+from fabric import task
+from invoke import Exit
+# import fabric.contrib.project as project
 import os
 import shutil
 import sys
-import SocketServer
+import socketserver
 
 from pelican.server import ComplexHTTPRequestHandler
 
 # Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'output'
-DEPLOY_PATH = env.deploy_path
-
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
+DEPLOY_PATH = "output"
 
 # Github Pages configuration
-env.github_pages_branch = "gh-pages"
+GITHUB_PAGES_BRANCH = "gh-pages"
 
 # Port for `serve`
 PORT = 8000
 
-def clean():
-    """Remove generated files"""
-    if os.path.isdir(DEPLOY_PATH):
-        shutil.rmtree(DEPLOY_PATH)
-        os.makedirs(DEPLOY_PATH)
+@task
+def clean(c):
+  """Remove generated files"""
+  if os.path.isdir(DEPLOY_PATH):
+    shutil.rmtree(DEPLOY_PATH)
+    os.makedirs(DEPLOY_PATH)
 
-def build():
-    """Build local version of site"""
-    local('pelican -s pelicanconf.py')
+@task
+def build(c):
+  """Build local version of site"""
+  c.run('pelican -s pelicanconf.py')
 
-def rebuild():
-    """`clean` then `build`"""
-    clean()
-    build()
+@task
+def rebuild(c):
+  """`clean` then `build`"""
+  clean(c)
+  build(c)
 
-def regenerate():
-    """Automatically regenerate site upon file modification"""
-    local('pelican -r -s pelicanconf.py')
+@task
+def regenerate(c):
+  """Automatically regenerate site upon file modification"""
+  c.local('pelican -r -s pelicanconf.py')
 
-def serve():
-    """Serve site at http://localhost:8000/"""
-    os.chdir(env.deploy_path)
+@task
+def serve(c):
+  """Serve site at http://localhost:8000/"""
+  os.chdir(DEPLOY_PATH)
 
-    class AddressReuseTCPServer(SocketServer.TCPServer):
-        allow_reuse_address = True
+  class AddressReuseTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
 
-    server = AddressReuseTCPServer(('', PORT), ComplexHTTPRequestHandler)
+  server = AddressReuseTCPServer(('', PORT), ComplexHTTPRequestHandler)
 
-    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
-    server.serve_forever()
+  sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+  server.serve_forever()
 
-def reserve():
-    """`build`, then `serve`"""
-    build()
-    serve()
+@task
+def reserve(c):
+  """`build`, then `serve`"""
+  build(c)
+  serve(c)
 
-def preview():
-    """Build production version of site"""
-    local('pelican -s publishconf.py')
+@task
+def preview(c):
+  """Build production version of site"""
+  c.local('pelican -s publishconf.py')
 
-def cf_upload():
-    """Publish to Rackspace Cloud Files"""
-    rebuild()
-    with lcd(DEPLOY_PATH):
-        local('swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-              '-U {cloudfiles_username} '
-              '-K {cloudfiles_api_key} '
-              'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
-def publish():
-    """Publish to production via rsync"""
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True,
-        extra_opts='-c',
-    )
-
-def gh_pages():
-    """Publish to GitHub Pages"""
-    clean()
-    local('pelican -s publishconf.py')
-    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
-    local("git push origin {github_pages_branch}".format(**env))
+@task
+def gh_pages(c):
+  """Publish to GitHub Pages"""
+  clean(c)
+  c.local('pelican -s publishconf.py')
+  c.local("ghp-import -b {} {}".format(GITHUB_PAGES_BRANCH, DEPLOY_PATH))
+  c.local("git push origin {}".format(GITHUB_PAGES_BRANCH))
